96b374c09578fcb33c367304f96de74b6a1b0ace
562690766 <562690766@qq.com>
2022-10-26 20:11:26 +0800

    模拟跨域实验开始

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin=require('mini-css-extract-plugin');
-const devMode=process.env.NODE_ENV!=='production';
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
 // 如果等于development就是true，否则就是false。
-const dev=process.env.NODE_ENV==="development";
+const dev = process.env.NODE_ENV === "development";
 module.exports = {
     entry: {
         index: "./src/index.js",
@@ -32,8 +32,8 @@ module.exports = {
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
-            filename:devMode?'[name].[hash].css':'[name].css',
-            chunkFilename: devMode?'[id].[hash].css':'[id].css',
+            filename: devMode ? '[name].[hash].css' : '[name].css',
+            chunkFilename: devMode ? '[id].[hash].css' : '[id].css',
         }),
     ],
     module: {
@@ -52,11 +52,11 @@ module.exports = {
             {
                 test: /\.css$/,
                 // 是开发者模式吗？是就用style-loader，不是就不用--并打包成独立文件
-                use: [dev?'style-loader':MiniCssExtractPlugin.loader, 'css-loader'],
+                use: [dev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
             }, {
                 test: /\.less$/,
                 use: [{
-                    loader:  dev?'style-loader':MiniCssExtractPlugin.loader // creates style nodes from JS strings
+                    loader: dev ? 'style-loader' : MiniCssExtractPlugin.loader // creates style nodes from JS strings
                 }, {
                     loader: 'css-loader' // translates CSS into CommonJS
                 }, {
@@ -65,7 +65,7 @@ module.exports = {
             }, {
                 test: /\.scss$/,
                 use: [{
-                    loader:  dev?'style-loader':MiniCssExtractPlugin.loader // creates style nodes from JS strings
+                    loader: dev ? 'style-loader' : MiniCssExtractPlugin.loader // creates style nodes from JS strings
                 }, {
                     loader: 'css-loader' // translates CSS into CommonJS
                 }, {
@@ -73,5 +73,18 @@ module.exports = {
                 }]
             }
         ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),//contentBase网站根目录
+        compress: true,//会 gzip(压缩) 和 serve(服务) 所有来自项目根路径下 dist/ 目录的文件
+        port: 9000,
+        proxy: {//代理服务器
+            "/data": { //地址
+                "target": "http://www.bjlink32.com/data.php", //接口地址,跨域访问
+                // secure: false,// 如果是https接口，需要配置这个参数
+                "changeOrigin": true,//开启跨域
+                "pathRewrite": { "^/data": "" }//如果接口本身没有/data需要通过pathRewrite来重写了地址
+            }
+        }
     }
 }
