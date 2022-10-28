9d53448e89ec9ab260de60410410ac0986415cf6
562690766 <562690766@qq.com>
2022-10-28 22:25:06 +0800

    方法三，遍历数组后返回一个新数组

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import React,{Component} from 'react';
 console.log("组件渲染了");
 class App extends Component{
-    render(){
-        // 写法二：
-        let arr=[
-            <li>susan</li>,
-            <li>tom</li>,
-            <li>jack</li>,
-            <li>张三</li>,
-        ];
-        return (//return返回一个对象的话要加小括号
-        //标签要小写，大写会当成组件
-            <div>
-                <ul>
-                {arr}
-                {/* 结果：·susan
-                          ·  tom
-                          · jack
-                          · 张三 */}
-                </ul>
-            </div>
-        )
-    }
+   render(){
+    // 方法三，遍历数组后返回一个新数组
+    let arr=[
+        "susan",
+        "张三  ",
+        "李四  ",
+        "马汉  ",
+    ]
+    return (
+        <div>
+            <ul>
+                {
+                    // map返回值就是数组
+                    arr.map((value,index)=>{
+                        return (
+                            <li key={index}>
+                                {value}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+   }
 }
 export default App;
