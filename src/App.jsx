2d860b46950c78b1cdaff990774303a3d51d89e8
562690766 <562690766@qq.com>
2022-10-28 20:51:15 +0800

    遍历方法二结束

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import React,{Component} from 'react';
 console.log("组件渲染了");
 class App extends Component{
     render(){
-        // 写法一：
-        let arr=["susan","tom","jack","张三"];
+        // 写法二：
+        let arr=[
+            <li>susan</li>,
+            <li>tom</li>,
+            <li>jack</li>,
+            <li>张三</li>,
+        ];
         return (//return返回一个对象的话要加小括号
         //标签要小写，大写会当成组件
             <div>
+                <ul>
                 {arr}
-                {/* 结果：susantomjack张三 */}
+                {/* 结果：·susan
+                          ·  tom
+                          · jack
+                          · 张三 */}
+                </ul>
             </div>
         )
     }
