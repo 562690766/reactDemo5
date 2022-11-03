9aa518a4f9292ccef5b58817a0f9493685473c5c
562690766 <562690766@qq.com>
2022-11-03 11:41:37 +0800

    键抬起重新render

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import { FileManager } from 'less';
+
 import React,{Component} from 'react'
 import Child from './Child'
 export default class App extends Component{
-     constructor(){
+    constructor (){ 
         super();
         this.state={
-            age:19,
-            num:0,
-            flag:true
+            text:""
         }
     }
-    handdleClick=()=>{
-        let {flag}=this.state;
-        this.setState({flag:!flag})
+    changeText=(event)=>{
+        console.log("changeText处理函数被调用了")
+        this.setState({
+            text:event.target.value
+        })
     }
-    render(){
-        let {age,num,flag}=this.state;
-        return (
-            <div>
-                <h3>App</h3>
-                <button onClick={()=>{
-                    this.setState({age:22,num:100})
-                }}>改变this.state.age</button>
-                {flag?<Child name="jack" age={age} num={num}></Child>:<div>哈哈哈</div>}
-                <button onClick={this.handdleClick}>change flag</button>
-            </div>
-        )
-    }
+        render(){
+            console.log("render ")
+            let {text}=this.state;
+            return(
+                <div>
+                    <h3>App组件</h3>
+                    <input type="text" onKeyUp={this.changeText}/>
+                    <p>{text}</p>
+                </div>
+            )
+        }
 }
+<App/>
+
--- src/Child.jsx
+++ src/Child.jsx
@@ -1,96 +1,21 @@
 import React,{Component} from 'react'
 export default class Child extends Component{
     constructor(){
-        console.log("constructor 0");
         super();
-        this.state={
-            count:0,
-            hours:0,
-            minutes:0,
-            seconds:0
-        }
-        this.sid=null;
+      this.myInput=React.createRef();
     }
-    showTime=()=>{
-        this.sid=setInterval(()=>{
-            let myDate=new Date();
-            let hours=myDate.getHours();
-            let minutes=myDate.getMinutes();
-            let seconds=myDate.getSeconds();
-            this.setState({
-                hours:hours,
-                minutes:minutes,
-                seconds:seconds
-            })
-            console.log("计时器正在执行");
-        },1000)
-    }
-    handdleClick=()=>{
-        let {count}=this.state;
-        this.setState({
-            count:count+1,
-        })
-    }
-    componentWillMount(){
-        console.log("componentWillMount 1");
-    }
-    render(){
-        console.log("render 2  7");
-        let {count,hours,minutes,seconds}=this.state;
-        return(
-            <div>
-                <h3>Child组件</h3>
-                <p>count:{count}</p>
-                <button onClick={this.handdleClick}>+</button>
-                <hr/>
-                <p>age:{this.props.age}</p>
-                <hr/>
-                <p>现在的时间是:{hours}:{minutes}:{seconds}</p>
-            </div>
-        )
-    }
-    componentDidMount(){
-        // 初始化的第三个方法
-        this.showTime();
-        window.onscroll=function(){
-            console.log(window.pageYOffset+"px");
-        }
-        console.log("componentDidMount 3")
-    }
-    // 第四步与更新无关，暂时屏蔽掉
-    componentWillReceiveProps(nextProps){
-        console.log("nextProps",nextProps,"this.props",this.props);
-        if(nextProps.num!=this.props.num){
-            this.setState({
-                count:nextProps.num
-            })
-        }
-        console.log("componentWillReceiveProps 4");
-        // {name:"jarry",age:22,count:100};
-    /*     console.log("nextProps",nextProps,"this.props",this.props)
-        if(nextProps.num!=this.props.num){
-            this.setState({//新的props值去修改当前组件Child的this.state.count
-                count:nextProps.num
-            })
-        } */
-    }
-    // 决定是否进入更新阶段，无论是否发生变化都会更新，解决方法：顶部导入的component改成PureComponent,再注释生命周期的第五个方法。
-    shouldComponentUpdate(nextProps,nextState){
-        console.log("shouldComponentUpdate 5");
-        return true;        
-    }
-    componentWillUpdate(){
-        console.log("componentWillUpdate 6");
-    }
-    componentDidUpdate(){
-        console.log("componentDidUpdate 8");
-    }
-    componentWillUnmount(){
-        clearInterval(this.sid);
-        window.onscroll=null;
-        console.log("componentWillUnmount 9");
-    }
-}
-Child.defaultProps={
-    name:"tom"
+   handdleClick=()=>{
+    console.log(this.myInput.current);
+    this.myInput.current.focus();
+    alert(this.myInput.current.value);
+   }
+   render(){
+    return(
+        <div>
+            <h3>App组件 React@16.3及以上 ref属性获取真实DOM对象</h3>
+            <input type="text" ref={this.myInput}/>
+            <button onClick={this.handdleClick}>点击获取input焦点</button>
+        </div>
+    )
+   }
 }
