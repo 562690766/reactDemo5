e14e7e951ac1cb599cd29096e047fd931840961f
562690766 <562690766@qq.com>
2022-11-03 14:21:19 +0800

    App React@16.2以下版本的ref属性 获取真实DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
-
-import React,{Component} from 'react'
-import Child from './Child'
+import React,{Component} from 'react';
 export default class App extends Component{
-    constructor (){ 
-        super();
-        this.state={
-            text:""
-        }
-    }
-    changeText=(event)=>{
-        console.log("changeText处理函数被调用了")
-        this.setState({
-            text:event.target.value
-        })
-    }
-        render(){
-            console.log("render ")
-            let {text}=this.state;
-            return(
-                <div>
-                    <h3>App组件</h3>
-                    <input type="text" onKeyUp={this.changeText}/>
-                    <p>{text}</p>
-                </div>
-            )
-        }
-}
-<App/>
+    haddleClick=()=>{
+        // this.refs的默认值是一个{}对象
+        // 点击后{}获取ref属性的DOM对象，变成this.refs{myInput:DOM对象}
+        // this.refs.myInput就是文本框对应的DOM对象
+        //console.log(this.refs.myInput);点击获取整个inout标签
+       // console.dir(this.refs.myInput)//点击获取真实DOM
+        this.refs.myInput.focus();//点击获取焦点
+        console.dir(this.refs.myInput.value)//点击获取文本框内容
 
+    }
+    render(){
+        return (
+            <div>
+                <h3>App React@16.2以下版本的ref属性 获取真实DOM </h3>
+                <input type="text" ref="myInput"/>
+                <button onClick={this.haddleClick}>点击获取</button>
+            </div>
+        )
+    }
+}
