0a91e12fbb112a80a33f4bfe91e980c70f66b31d
562690766 <562690766@qq.com>
2022-11-03 16:36:15 +0800

    App React@16.2以上版本的ref属性 获取真实DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React,{Component} from 'react';
 export default class App extends Component{
+    constructor(){
+        super();
+        // 创建一个ref对象
+        // 挂到组件实例对象上的自定义属性
+        // 挂载this上，组件内各个函数就能拿到
+        this.myInput=React.createRef();//拥有默认属性{current:null}
+    }
     haddleClick=()=>{
-        // this.refs的默认值是一个{}对象
-        // 点击后{}获取ref属性的DOM对象，变成this.refs{myInput:DOM对象}
-        // this.refs.myInput就是文本框对应的DOM对象
-        //console.log(this.refs.myInput);点击获取整个inout标签
-       // console.dir(this.refs.myInput)//点击获取真实DOM
-        this.refs.myInput.focus();//点击获取焦点
-        console.dir(this.refs.myInput.value)//点击获取文本框内容
-
+        console.log(this.myInput.current);//获取真实DOM
+        this.myInput.current.focus();
+        console.log(this.myInput.current.value);
     }
     render(){
         return (
             <div>
-                <h3>App React@16.2以下版本的ref属性 获取真实DOM </h3>
-                <input type="text" ref="myInput"/>
+                <h3>App React@16.2以上版本的ref属性 获取真实DOM </h3>
+                <input type="text" ref={this.myInput}/>
                 <button onClick={this.haddleClick}>点击获取</button>
             </div>
         )
